e6d5a783264afac5c454bb2a5888c637bf2a20f9
helix_xx <630824252@qq.com>
2023-07-25 15:15:10 +0800

    add run-test.sh

---
+++ molecular-design/run-test--no-ps.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+search_space=../data/moldesign/search-space/MOS-search.csv
+
+# Get the MPNN models
+data_dir=../data/moldesign/
+mpnn_file=$data_dir/initial-model/networks/gpu_b16_n256_Rsum_cbd46e/model.h5
+
+
+# Relevant endpoints
+#  b9c5db67-cc17-4708-be97-5274443d789a: Debug queue, one node, flat memory
+#  acdb2f41-fd86-4bc7-a1e5-e19c12d3350d: Lambda
+#  de92a1ac-4118-48a2-90ac-f43a59298634: Venti
+
+python run.py \
+       --ml-endpoint de92a1ac-4118-48a2-90ac-f43a59298634 \
+       --qc-endpoint b9c5db67-cc17-4708-be97-5274443d789a \
+       --redisport 7485 \
+       --training-set $data_dir/training-data.json \
+       --mpnn-model-path $mpnn_file \
+       --model-count 2 \
+       --num-epochs 32 \
+       --search-space $search_space \
+       --infer-ps-backend redis \
+       --train-ps-backend redis \
+       --simulate-ps-backend file \
+       --ps-file-dir proxy-store-scratch \
+       --ps-globus-config globus_config.json \
+       --num-qc-workers 8 \
+       --retrain-frequency 1 \
+       --molecules-per-ml-task 1000 \
+       --search-size 128 \
+       --ps-threshold 1000 \
+       --use-parsl \
+       --no-proxystore
