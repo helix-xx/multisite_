832696e790c6b36c9873eb0ba821238b0994f7c4
Yuan xuxuan <630824252@qq.com>
2023-08-03 03:44:58 +0800

    modify config for scale

--- configs/config.py
+++ configs/config.py
@@ -203,6 +203,56 @@ def csecluster_RT(log_dir: str) -> Config:
     return config
 
 
+def csecluster_RT_scale(log_dir: str) -> Config:
+    config = Config(
+        run_dir=log_dir,
+        retries=1,
+        executors=[
+            HighThroughputExecutor(
+                label="cpu",
+                max_workers=8,
+                address=address_by_hostname(),
+                worker_port_range=(20000,30000),
+                worker_logdir_root='/home/lizz_lab/cse12232433/parsl-logs',
+                provider=AdHocProvider(
+                    min_blocks=1,
+                    max_blocks=4,
+                    init_blocks=1,
+                    channels=[SSHChannel(hostname='gpu001',port='22', username='cse12232433', password='Yxx!199871!', script_dir='/home/lizz_lab/cse12232433/parsl-logs')],
+                    worker_init='''
+                    # Activate conda environment
+                    source /home/lizz_lab/cse12232433/.bashrc
+                    source /home/lizz_lab/cse12232433/miniconda3/bin/activate /home/lizz_lab/cse12232433/miniconda3/envs/multisite
+                    export CUDA_VISIBLE_DEVICES=0,1,2,3
+                    which python
+                    ''',
+                ),
+            ),
+            HighThroughputExecutor(
+                address=address_by_hostname(),
+                label="gpu",
+                available_accelerators=1,
+                worker_port_range=(20000,30000),
+                worker_logdir_root='/home/lizz_lab/cse12232433/parsl-logs',
+                provider=AdHocProvider(
+                    min_blocks=1,
+                    max_blocks=4,
+                    init_blocks=1,
+                    channels=[SSHChannel(hostname='gpu001',port='22', username='cse12232433', password='Yxx!199871!', script_dir='/home/lizz_lab/cse12232433/parsl-logs')],
+                    worker_init='''
+                    # Activate conda environment
+                    source /home/lizz_lab/cse12232433/.bashrc
+                    source /home/lizz_lab/cse12232433/miniconda3/bin/activate /home/lizz_lab/cse12232433/miniconda3/envs/multisite
+                    # export TF_GPU_ALLOCATOR=cuda_malloc_async
+                    which python
+                    ''',
+                ),
+            )]
+    )
+        
+    return config
+
+
 def wsl(log_dir: str) -> Config:
     config = Config(
         run_dir=log_dir,
