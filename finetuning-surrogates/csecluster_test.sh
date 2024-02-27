29eb2ca83bd1af8e1878b7ce16b61664b3910a15
yxx-helix <630824252@qq.com>
2024-02-27 16:08:36 +0800

    sample running informations for train predict model

--- finetuning-surrogates/csecluster_test.sh
+++ finetuning-surrogates/csecluster_test.sh
@@ -39,29 +39,31 @@ fi
     
     ## redisport 7485->7486(test)
 
-    ## baseline
-python run_test_wq.py \
-  --ml-endpoint db55e9cc-ec32-47d6-a6ff-ecd45776d276 \
-  --qc-endpoint 698fba9a-4b12-4e0b-b83a-be6ded509946 \
-  --training-set ../data/forcefields/starting-model/initial-database.db \
-  --search-space ../data/forcefields/starting-model/initial-database.db \
-  --starting-model ../data/forcefields/starting-model/starting-model \
-  --num-qc-workers 8 \
-  --min-run-length 200 \
-  --max-run-length 2000 \
-  --num-frames 100 \
-  --num-epochs 1 \
-  --ensemble-size 2 \
-  --huber-deltas 1 10 \
-  --infer-chunk-size 4000 \
-  --infer-pool-size 1 \
-  --retrain-freq 16 \
-  --num-to-run 20 \
-  --parsl \
-  --no-proxies \
-  --redisport 7485 \
-  --calculator dft \
-  --train-ps-backend redis \
-  --sampling-on-device gpu \
-  --work-dir "$work_dir" \
-  --threads 32 \
+threads=("64" "32" "16" "8" "4" "2" "1")
+for thread in ${threads[@]}; do
+    python run_test.py \
+        --ml-endpoint db55e9cc-ec32-47d6-a6ff-ecd45776d276 \
+        --qc-endpoint 698fba9a-4b12-4e0b-b83a-be6ded509946 \
+        --training-set ../data/forcefields/starting-model/initial-database.db \
+        --search-space ../data/forcefields/starting-model/initial-database.db \
+        --starting-model ../data/forcefields/starting-model/starting-model \
+        --num-qc-workers 8 \
+        --min-run-length 200 \
+        --max-run-length 2000 \
+        --num-frames 100 \
+        --num-epochs 1 \
+        --ensemble-size 2 \
+        --huber-deltas 1 10 \
+        --infer-chunk-size 4000 \
+        --infer-pool-size 1 \
+        --retrain-freq 16 \
+        --num-to-run 20 \
+        --parsl \
+        --no-proxies \
+        --redisport 7485 \
+        --calculator dft \
+        --train-ps-backend redis \
+        --sampling-on-device gpu \
+        --work-dir "$work_dir" \
+        --threads $thread
+done
