f8cc96e477a85b428524db62f08685f3ab00c3ce
Yuan xuxuan <630824252@qq.com>
2024-12-10 20:00:53 +0800

    4 nodes experiments

--- finetuning-surrogates/cse_RT_slurm_launch.sh
+++ finetuning-surrogates/cse_RT_slurm_launch.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=gpulab01      # 作业提交的指定分区队列为titan
 #SBATCH --qos=gpulab01            # 指定作业的QOS
 #SBATCH -J finetuning-surrogates       # 作业在调度系统中的作业名为myFirstJob;
-#SBATCH --nodes=2              # 申请节点数为1,如果作业不能跨节点(MPI)运行, 申请的节点数应不超过1
-#SBATCH --ntasks-per-node=56    # 每个节点上运行一个任务，默认一情况下也可理解为每个节点使用一个核心；
-#SBATCH --gres=gpu:4           # 指定作业的需要的GPU卡数量，集群不一样，注意最大限制; 
+#SBATCH --nodes=4              # 申请节点数为1,如果作业不能跨节点(MPI)运行, 申请的节点数应不超过1
+#SBATCH --ntasks-per-node=28    # 每个节点上运行一个任务，默认一情况下也可理解为每个节点使用一个核心；
+#SBATCH --gres=gpu:2           # 指定作业的需要的GPU卡数量，集群不一样，注意最大限制; 
 #SBATCH --time=4:00:00       
 
 ## description here 
